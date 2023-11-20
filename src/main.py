4f594a55afaf90036363cb3341c21c2e81e4612e
R1DF <33981824+R1DF@users.noreply.github.com>
2023-11-20 20:33:09 +0600

    Created main run function

---
+++ src/main.py
@@ -0,0 +1,24 @@
+# Imports
+from config_reader import ConfigReader
+from qreplace import QReplace
+
+# Constants
+VERSION = "0.0.0"
+SAFE = True  # Set to false to disable error handler
+
+# Run function
+def run():
+    configurations = ConfigReader()
+    QReplace(VERSION, configurations)
+
+
+# Running program
+if __name__ == "__main__":
+    # Safe run
+    if SAFE:
+        try:
+            run()
+        except Exception as exception:
+            pass
+    else:
+        run()

2a6e2762c67a398d221c26966596a916e5cbadc8
R1DF <33981824+R1DF@users.noreply.github.com>
2023-11-22 20:00:01 +0600

    Added iconphoto loading as well as prefix and suffix entries

--- src/qreplace.py
+++ src/qreplace.py
@@ -1,7 +1,9 @@
 # Imports
-from tkinter import Tk, Frame, Label, Button
+from tkinter import Tk, Frame, Entry, Label, Button, Menu, PhotoImage
 from config_reader import ConfigReader
 from replacer_listbox import ReplacerListbox
+import os
+
 
 # QReplace class
 class QReplace(Tk):
@@ -10,7 +12,30 @@ class QReplace(Tk):
         super().__init__()
         self.configurations = configurations
         self.title(f"qReplace v{version}")
+        self.resizable(False, False)
         self.geometry(f"{self.configurations.get('resolution')[0]}x{self.configurations.get('resolution')[1]}")
+
+        # Setting icon
+        self.icon_photoimage = PhotoImage(file=os.path.join(os.getcwd(), "assets", "icon_32x32.png"))
+        self.iconphoto(True, self.icon_photoimage)
+
+        # Laying out menu
+        self.main_menu_bar = Menu(self, tearoff=0)
+        self.file_menu = Menu(self.main_menu_bar, tearoff=0)
+        self.file_menu.add_command(label="New")
+        self.file_menu.add_command(label="Reset")
+        self.file_menu.add_command(label="Open")
+        self.file_menu.add_command(label="Open recent")
+        self.main_menu_bar.add_cascade(label="File", menu=self.file_menu)
+
+        self.qreplace_menu = Menu(self.main_menu_bar, tearoff=0)
+        self.qreplace_menu.add_command(label="Preferences")
+        self.qreplace_menu.add_command(label="About")
+        self.qreplace_menu.add_separator()
+        self.qreplace_menu.add_command(label="Check for updates")
+        self.main_menu_bar.add_cascade(label="qReplace", menu=self.qreplace_menu)
+
+        self.config(menu=self.main_menu_bar)
 
         # Laying out widgets
         self.introduction_label = Label(self, text="qReplace")
@@ -25,8 +50,24 @@ class QReplace(Tk):
         self.right_frame = Frame(self.main_frames_frame)
         self.right_frame.grid(row=0, column=1, padx=15)
 
-        self.replacer_listbox = ReplacerListbox(self.left_frame, width=35)
-        self.replacer_listbox.pack()
+        self.replacer_listbox = ReplacerListbox(self.left_frame, width=35, height=12)
+
+        self.entries_frame = Frame(self.right_frame)
+        self.entries_frame.pack()
+
+        self.prefix_label = Label(self.entries_frame, text="Prefix:")
+        self.prefix_label.grid(row=0, column=0)
+
+        self.prefix_entry = Entry(self.entries_frame)
+        self.prefix_entry.grid(row=0, column=1)
+        self.prefix_entry.insert("end", self.configurations.get("default_prefix"))
+
+        self.suffix_label = Label(self.entries_frame, text="Suffix:")
+        self.suffix_label.grid(row=1, column=0)
+
+        self.suffix_entry = Entry(self.entries_frame)
+        self.suffix_entry.grid(row=1, column=1)
+        self.suffix_entry.insert("end", self.configurations.get("default_suffix"))
 
         self.buttons_frame = Frame(self.right_frame)
         self.buttons_frame.pack()

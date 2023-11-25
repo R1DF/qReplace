2b899aff29ba7bd9e5a04b7462962b047bd537e4
R1DF <33981824+R1DF@users.noreply.github.com>
2023-11-25 14:56:14 +0600

    Nearly completed toplevels

--- src/toplevels.py
+++ src/toplevels.py
@@ -4,9 +4,12 @@ from tkinter import Tk, Toplevel, Frame, Entry, Label, Button
 # Base toplevel
 class BaseToplevel(Toplevel):
     def __init__(self, master: Tk, name: str):
+        # Initialisation
         super().__init__(master)
         self.master = master
         self.name = name
+
+        # Configuration
         self.master.child_toplevels[self.name] = True
         self.protocol("WM_DELETE_WINDOW", self.handle_close)
 
@@ -19,11 +22,72 @@ class BaseToplevel(Toplevel):
 class AddToplevel(BaseToplevel):
     def __init__(self, master):
         super().__init__(master, "add")
+        # Initialisation
         self.title("Add entry")
+        self.geometry("225x100")
+        self.resizable(False, False)
+
+        # Widget creation
+        self.introduction_label = Label(self, text="Add new entry:")
+        self.introduction_label.pack()
+
+        self.information_frame = Frame(self)
+        self.information_frame.pack()
+
+        self.phrase_label = Label(self.information_frame, text="Phrase:")
+        self.phrase_label.grid(row=0, column=0)
+
+        self.phrase_entry = Entry(self.information_frame)
+        self.phrase_entry.grid(row=0, column=1)
+
+        self.replacement_label = Label(self.information_frame, text="Replacement:")
+        self.replacement_label.grid(row=1, column=0)
+
+        self.replacement_entry = Entry(self.information_frame)
+        self.replacement_entry.grid(row=1, column=1)
+
+        self.add_button = Button(self, text="Add", command=self.handle_add)
+        self.add_button.pack()
+
+    def handle_add(self):
+        self.handle_close()
 
 
 # Edit toplevel
 class EditToplevel(BaseToplevel):
-    def __init__(self, master):
+    def __init__(self, master, index: int, original_phrase: str, original_replacement: str):
         super().__init__(master, "edit")
+        # Initialisation
+        self.entry_index = index
+        self.original_phrase = original_phrase
+        self.original_replacement = original_replacement
         self.title("Edit entry")
+        self.geometry("225x100")
+        self.resizable(False, False)
+
+        # Widget creation
+        self.introduction_label = Label(self, text="Edit entry:")
+        self.introduction_label.pack()
+
+        self.information_frame = Frame(self)
+        self.information_frame.pack()
+
+        self.phrase_label = Label(self.information_frame, text="Phrase:")
+        self.phrase_label.grid(row=0, column=0)
+
+        self.phrase_entry = Entry(self.information_frame)
+        self.phrase_entry.grid(row=0, column=1)
+        self.phrase_entry.insert("end", original_phrase)
+
+        self.replacement_label = Label(self.information_frame, text="Replacement:")
+        self.replacement_label.grid(row=1, column=0)
+
+        self.replacement_entry = Entry(self.information_frame)
+        self.replacement_entry.grid(row=1, column=1)
+        self.phrase_entry.insert("end", original_replacement)
+
+        self.edit_button = Button(self, text="Edit", command=self.handle_edit)
+        self.edit_button.pack()
+
+    def handle_edit(self):
+        self.handle_close()

69fa9f550a80c61681764a1ecf11cfb4c7b133f6
R1DF <33981824+R1DF@users.noreply.github.com>
2023-11-26 14:38:04 +0600

    Completed main functionality

--- src/qreplace.py
+++ src/qreplace.py
@@ -1,8 +1,9 @@
 # Imports
-from tkinter import Tk, Frame, Entry, Label, Button, Menu, PhotoImage, messagebox
+from tkinter import Tk, Frame, Entry, Label, Button, Menu, PhotoImage, messagebox, filedialog
 from config_reader import ConfigReader
 from replacer_listbox import ReplacerListbox
 from toplevels import AddToplevel, EditToplevel
+from file_manager import save_list_as_ahk, open_ahk_as_dict
 import os
 
 
@@ -16,10 +17,12 @@ class QReplace(Tk):
             "edit": False,
             "about": False
         }
+        self.version = version
         self.configurations = configurations
         self.title(f"qReplace v{version}")
         self.resizable(False, False)
         self.geometry(f"{self.configurations.get('resolution')[0]}x{self.configurations.get('resolution')[1]}")
+        self.recent_files = []
 
         # Setting icon
         self.icon_photoimage = PhotoImage(file=os.path.join(os.getcwd(), "assets", "icon_32x32.png"))
@@ -28,17 +31,16 @@ class QReplace(Tk):
         # Laying out menu
         self.main_menu_bar = Menu(self, tearoff=0)
         self.file_menu = Menu(self.main_menu_bar, tearoff=0)
-        self.file_menu.add_command(label="New")
-        self.file_menu.add_command(label="Reset")
-        self.file_menu.add_command(label="Open")
-        self.file_menu.add_command(label="Open recent")
+        self.file_menu.add_command(label="New", command=self.handle_new)
+        self.file_menu.add_command(label="Open", command=self.handle_open)
+        self.file_menu.add_command(label="Open recent", command=self.handle_open_recent)
         self.main_menu_bar.add_cascade(label="File", menu=self.file_menu)
 
         self.qreplace_menu = Menu(self.main_menu_bar, tearoff=0)
-        self.qreplace_menu.add_command(label="Preferences")
-        self.qreplace_menu.add_command(label="About")
+        self.qreplace_menu.add_command(label="Preferences", command=self.handle_preferences)
+        self.qreplace_menu.add_command(label="About", command=self.handle_about)
         self.qreplace_menu.add_separator()
-        self.qreplace_menu.add_command(label="Check for updates")
+        self.qreplace_menu.add_command(label="Check for updates", command=self.handle_check_for_updates)
         self.main_menu_bar.add_cascade(label="qReplace", menu=self.qreplace_menu)
 
         self.config(menu=self.main_menu_bar)
@@ -124,4 +126,70 @@ class QReplace(Tk):
         self.replacer_listbox.remove_item(entry_index[0])
 
     def handle_save(self):
+        if self.replacer_listbox.is_empty:
+            messagebox.showerror("Error", "Please add an entry to the list before saving.")
+            return
+
+        # Getting data
+        prefix = self.prefix_entry.get().strip()
+        suffix = self.suffix_entry.get().strip()
+        if not prefix:
+            messagebox.showerror("Error", "A prefix is required.")
+            return
+
+        else:
+            path = filedialog.asksaveasfilename(filetypes=[
+                    ("AutoHotKey file", "*.ahk")
+                ]
+            )
+
+            if path:
+                # Saving data to .ahk file
+                save_list_as_ahk(path, prefix, suffix, self.version, self.replacer_listbox.contents)
+
+    def handle_new(self):
+        self.prefix_entry.delete(0, "end")
+        self.suffix_entry.delete(0, "end")
+        self.replacer_listbox.erase()
+
+    def handle_open(self):
+        path = filedialog.askopenfilename(filetypes=[
+                    ("AutoHotKey file", "*.ahk")
+                ]
+            )
+        if path:
+            # Getting data from function
+            file_data = open_ahk_as_dict(path)
+            prefix = file_data["prefix"]
+            suffix = file_data["suffix"]
+            replacers_list = file_data["replacers"]
+
+            # Adding to recent (or putting to top)
+            if path not in self.recent_files:
+                self.recent_files.append(path)
+            else:
+                self.recent_files.remove(path)
+                self.recent_files.append(path)
+
+            # Setting up prefixes
+            self.prefix_entry.delete(0, "end")
+            self.suffix_entry.delete(0, "end")
+            self.prefix_entry.insert("end", prefix)
+            self.suffix_entry.insert("end", suffix)
+
+            # Copying replacers
+            self.replacer_listbox.erase()
+            for phrase, suffix in replacers_list:
+                self.replacer_listbox.add_item(phrase, suffix)
+
+    def handle_open_recent(self):
+        pass
+
+    def handle_preferences(self):
+        pass
+
+    def handle_about(self):
+        pass
+
+    def handle_check_for_updates(self):
         pass
